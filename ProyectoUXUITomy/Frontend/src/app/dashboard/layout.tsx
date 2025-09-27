1981573b9df5b2b7597b60f12c647eb79f0bb78a
InZenFenix <108585945+inzenfenix@users.noreply.github.com>
2025-09-27 19:08:02 -0300

    Revert "Update layout.tsx"
    This reverts commit 0b1b6336575eade7b5b6ddef2e721bc09e682ca7.

--- ProyectoUXUITomy/Frontend/src/app/dashboard/layout.tsx
+++ ProyectoUXUITomy/Frontend/src/app/dashboard/layout.tsx
@@ -47,8 +47,6 @@ export default function DashboardLayout({
     const refreshToken = localStorage.getItem("refreshToken");
     if(refreshToken)
       RefreshSession(refreshToken);
-    else
-      router.push("/");
   }, []);
 
   return (
