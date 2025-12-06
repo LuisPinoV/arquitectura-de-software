6cd0049e3825ac9ba753d85d87a8de6749955466
InZenFenix <108585945+inzenfenix@users.noreply.github.com>
2025-12-06 20:51:32 -0300

    Update app-sidebar.tsx

--- Frontend/src/components/app-sidebar.tsx
+++ Frontend/src/components/app-sidebar.tsx
@@ -150,14 +150,14 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
                   <Image
                     src=""
-                    alt={"Bolsa con un más"}
+                    alt={"ICO"}
                     height={20}
                     width={20}
                   />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate text-lg font-medium">
-                    {companyName ?? "AIOHospital"}
+                    {companyName ?? "Agendín"}
                   </span>
                 </div>
               </a>
