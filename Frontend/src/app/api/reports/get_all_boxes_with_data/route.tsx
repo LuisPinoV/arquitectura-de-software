77572358a031cbe80ea4c15a278d88ae07c7b321
InZenFenix <108585945+inzenfenix@users.noreply.github.com>
2025-12-08 16:52:02 -0300

    Update route.tsx

--- Frontend/src/app/api/reports/get_all_boxes_with_data/route.tsx
+++ Frontend/src/app/api/reports/get_all_boxes_with_data/route.tsx
@@ -2,51 +2,55 @@ import { NextRequest, NextResponse } from "next/server";
 import { Box } from "../../../dashboard/reportaje/components/table-boxes";
 
 export async function GET(req: NextRequest) {
+  const today = new Date();
+  const todayDateISO = today.toISOString().split("T")[0];
+  const todayTimeISO = today.toISOString().split("T")[1];
 
-    const today = new Date();
-    const todayDateISO = today.toISOString().split("T")[0];
-    const todayTimeISO = today.toISOString().split("T")[1];
-
-    const apiUrl = process.env.BACKEND_ADDRESS;
+  const apiUrl = process.env.BACKEND_ADDRESS;
 
   const incomingToken = req.headers.get("authorization") ?? "";
 
-  const resAll = await fetch(
-    `${apiUrl}/box`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": incomingToken,   // <-- Forward it to backend
-      },
-    }
-  );
+  const resAll = await fetch(`${apiUrl}/box`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: incomingToken, // <-- Forward it to backend
+    },
+  });
 
   const dataAll = await resAll.json();
 
-  const dataBoxes:Box[] = [];
+  console.log(dataAll);
 
+  const dataBoxes: Box[] = [];
 
-  for(let i = 0; i < dataAll.length; i++)
-  {
-    const resPerBox = await fetch(
-    `${apiUrl}/usoBox/${dataAll[i]["idbox"]}/${todayDateISO}/${todayTimeISO}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": incomingToken,   // <-- Forward it to backend
-      },
+  if (Array.isArray(dataAll)) {
+    if (dataAll.length == 0) {
+      return NextResponse.json([]);
     }
-  );
-    const dataPerBox = await resPerBox.json();
-
-    dataBoxes.push({
-        id: dataPerBox["id"],
-        state:dataPerBox["libre"] ? "Libre" : "Ocupado",
-        usage: parseFloat(dataPerBox["ocupancia"].toFixed(1)) * 100
-    })
-
   }
 
-  
+  for (let i = 0; i < dataAll.length; i++) {
+    const resPerBox = await fetch(
+      `${apiUrl}/usoBox/${dataAll[i]["idBox"]}/${todayDateISO}/${todayTimeISO}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: incomingToken, // <-- Forward it to backend
+        },
+      }
+    );
+    const dataPerBox = await resPerBox.json();
+
+    console.log(dataPerBox);
+
+    if (dataPerBox && dataPerBox.message !== 'Not Found') {
+      dataBoxes.push({
+        id: dataPerBox["id"],
+        state: dataPerBox["libre"] ? "Libre" : "Ocupado",
+        usage: parseFloat(dataPerBox["ocupancia"].toFixed(1)) * 100,
+      });
+    }
+  }
+
   return NextResponse.json(dataBoxes);
 }
